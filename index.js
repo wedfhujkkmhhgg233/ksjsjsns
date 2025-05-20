7e23ac327224312b47e48391d8501a90cdb6fdec
wedfhujkkmhhgg233 <154974042+wedfhujkkmhhgg233@users.noreply.github.com>
2025-05-21 07:34:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -327,7 +327,6 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
